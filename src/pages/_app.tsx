c82d8a02d1244abf9766a064c8f0f259381b136a
Max Programming <ui.sabuwala@gmail.com>
2022-11-11 00:44:00 +0530

    Added Progress bar while navigating

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,22 @@
 import "../styles/globals.css";
 import type { AppType } from "next/app";
 import { trpc } from "../utils/trpc";
+import NextNProgress from "nextjs-progressbar";
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import daisyuiColors from "daisyui/src/colors/themes";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <NextNProgress
+        options={{ showSpinner: false }}
+        color={daisyuiColors["[data-theme=light]"].primary}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default trpc.withTRPC(MyApp);
