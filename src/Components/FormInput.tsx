a16011d48c9e4509be9ba89f28587c3745cca1b8
Anon <opss59332@gmail.com>
2022-11-26 02:57:17 +0530

    date and passenger count done

--- src/Components/FormInput.tsx
+++ src/Components/FormInput.tsx
@@ -1,22 +1,25 @@
-import { useRef, useState } from "react";
-// import { DateRangePicker, DateRangePickerValue } from "@mantine/dates";
+import { useState, useRef } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 function FormInput() {
-  // const [value, setValue] = useState<DateRangePickerValue>([
-  //   new Date(2021, 11, 1),
-  //   new Date(2021, 11, 5),
-  // ]);
+  const [departureDate, setDepartureDate] = useState();
+  const [returnDate, setReturnDate] = useState();
+  const [roundTrip, setRoundTrip] = useState();
+  const [showPassengerCountModal, setShowPassengerCountModal] = useState(false);
+  const [adultCount, setAdultCount] = useState(1);
+  const [childrenCount, setChilrenCount] = useState(0);
 
-  // const Dateref = useRef();
-  const [startDate, setStartDate] = useState(new Date());
-  
+  function tripType(e) {
+    if (e.target.value === "roundTripType") {
+      setRoundTrip(true);
+    } else if (e.target.value === "oneWayTripType") {
+      setRoundTrip(false);
+    }
+  }
 
   return (
-
     <section className="flex h-screen w-screen flex-col justify-center bg-[url('/images/flightbg.png')] bg-cover bg-center">
-
       <div className="ml-36 mb-10 max-w-[40ch]">
         <h1 className="font-['ubuntu'] text-5xl leading-relaxed text-white">
           Let the journey begin....
@@ -46,30 +49,143 @@ function FormInput() {
               </option>
             </select>
 
-            {/* <input
-              type="date"
-              className="align-center broder-solid h-12 w-[242px] rounded-l-md border-r-2 border-black p-2"
-            /> */}
-            <div className="w-[242px]">
-              <label htmlFor="">
-                <input type="radio" name="trip-type" id="" /> Round Trip
-              </label>
-              <label htmlFor="">
-                <input type="radio" name="trip-type" id="" /> One Way
-              </label>
-
-            <DatePicker selected={startDate} onChange={(date) => setStartDate(date)}/>
-
-              {/* <DateRangePicker
-                ref={Dateref}
-                placeholder="Pick dates range"
-                value={value}
-                onChange={setValue}
-              /> */}
+            <div className="flex w-[242px] flex-row justify-evenly">
+              <DatePicker
+                className="w-[110px] border-r-2 p-2 focus:outline-0"
+                placeholderText="Departure date"
+                selected={departureDate}
+                onChange={(date) => setDepartureDate(date)}
+                renderCustomHeader={() => {
+                  return (
+                    <div className="flex flex-row justify-evenly">
+                      <label htmlFor="">
+                        <input
+                          type="radio"
+                          name="trip-type"
+                          id=""
+                          value={"roundTripType"}
+                          onChange={tripType}
+                        />{" "}
+                        Round Trip
+                      </label>
+                      <label htmlFor="">
+                        <input
+                          type="radio"
+                          name="trip-type"
+                          id=""
+                          value={"oneWayTripType"}
+                          onChange={tripType}
+                        />{" "}
+                        One Way
+                      </label>
+                    </div>
+                  );
+                }}
+              />
+              <DatePicker
+                className={
+                  !roundTrip
+                    ? "hidden"
+                    : "w-[110px] border-r-2 p-2 focus:outline-0"
+                }
+                placeholderText="Return date"
+                selected={returnDate}
+                onChange={(date) => setReturnDate(date)}
+              />
             </div>
-            <button className="align-center h-12 w-[240px] rounded-l-md p-2">
-              1 Adult
-            </button>
+            <div>
+              <button
+                onClick={() => {
+                  setShowPassengerCountModal(!showPassengerCountModal);
+                }}
+                className="align-center h-12 w-[240px] rounded-l-md p-2"
+              >
+                {adultCount} Adult {childrenCount} Children
+              </button>
+
+              <div
+                className={
+                  !showPassengerCountModal
+                    ? "hidden"
+                    : "absolute ml-8 flex h-16 w-[200px] flex-col justify-around bg-white"
+                }
+              >
+                <div className="relative flex w-[200px] flex-row justify-around">
+                  <button
+                    onClick={() => {
+                      adultCount > 1 ? setAdultCount(adultCount - 1) : "";
+                    }}
+                  >
+                    <svg
+                      className="cursor-pointer"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      width="24"
+                      height="24"
+                    >
+                      <path fill="none" d="M0 0h24v24H0z" />
+                      <path d="M5 11h14v2H5z" />
+                    </svg>
+                  </button>
+                  <p>Adult</p>
+                  <button
+                    onClick={() => {
+                      setAdultCount(adultCount + 1);
+                    }}
+                  >
+                    <svg
+                      className="cursor-pointer"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      width="24"
+                      height="24"
+                    >
+                      <path fill="none" d="M0 0h24v24H0z" />
+                      <path d="M11 11V5h2v6h6v2h-6v6h-2v-6H5v-2z" />
+                    </svg>
+                  </button>
+                </div>
+
+                <div className="relative flex w-[200px] flex-row justify-around">
+                  <button
+                    onClick={() => {
+                      setChilrenCount(childrenCount - 1);
+                    }}
+                  >
+                    <svg
+                      className="cursor-pointer"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      width="24"
+                      height="24"
+                    >
+                      <path fill="none" d="M0 0h24v24H0z" />
+                      <path d="M5 11h14v2H5z" />
+                    </svg>
+                  </button>
+
+                  <p>Children</p>
+
+                  <button
+                    onClick={() => {
+                      setChilrenCount(childrenCount + 1);
+                    }}
+                  >
+                    {" "}
+                    <svg
+                      className="cursor-pointer"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      width="24"
+                      height="24"
+                    >
+                      <path fill="none" d="M0 0h24v24H0z" />
+                      <path d="M11 11V5h2v6h6v2h-6v6h-2v-6H5v-2z" />
+                    </svg>
+                  </button>
+                </div>
+              </div>
+            </div>
 
             <button className="align-center h-12 w-[240px] rounded-r-md bg-[#007CFF] p-2 text-white">
               Search
