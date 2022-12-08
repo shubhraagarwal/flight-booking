a44c59d7d8b8a72985aeba786ff0aa4e273afaa1
Max Programming <ui.sabuwala@gmail.com>
2022-12-08 11:55:59 +0530

    Refactor component

--- src/components/FlightsPage/FlightInfo.tsx
+++ src/components/FlightsPage/FlightInfo.tsx
@@ -1,61 +1,35 @@
 function FlightInfo() {
   return (
     <section className="w-[60vw]">
-      <div className="flex h-24 flex-row items-center justify-between">
-        <div className="">
-          <img src="/images/flightLogo.png" alt="" />
-        </div>
-        <div className="flex h-24 flex-col justify-around text-left">
-          <p>16h 23M</p> <p>Hawaiian Airlines</p>
-        </div>
-        <div className="flex h-24 flex-col justify-around text-left">
-          <p>7:00AM - 4:15PM</p> <p>Value</p>
-        </div>
-        <div className="flex h-24 flex-col justify-around text-right">
-          <p>1 stop</p> <p>2h 45m in HNL</p>
-        </div>
-        <div className="flex h-24 flex-col justify-around text-right">
-          <p>Rs. 5894</p> <p>single trip</p>
-        </div>
-      </div>
-      <hr />
-      <div className="flex h-24 flex-row items-center justify-between">
-        <div className="">
-          <img src="/images/flightLogo.png" alt="" />
-        </div>
-        <div className="flex h-24 flex-col justify-around text-left">
-          <p>16h 23M</p> <p>Hawaiian Airlines</p>
-        </div>
-        <div className="flex h-24 flex-col justify-around text-left">
-          <p>7:00AM - 4:15PM</p> <p>Value</p>
-        </div>
-        <div className="flex h-24 flex-col justify-around text-right">
-          <p>1 stop</p> <p>2h 45m in HNL</p>
-        </div>
-        <div className="flex h-24 flex-col justify-around text-right">
-          <p>Rs. 5894</p> <p>single trip</p>
-        </div>
-      </div>
-      <hr />
-      <div className="flex h-24 flex-row items-center justify-between">
-        <div className="">
-          <img src="/images/flightLogo.png" alt="" />
-        </div>
-        <div className="flex h-24 flex-col justify-around text-left">
-          <p>16h 23M</p> <p>Hawaiian Airlines</p>
-        </div>
-        <div className="flex h-24 flex-col justify-around text-left">
-          <p>7:00AM - 4:15PM</p> <p>Value</p>
-        </div>
-        <div className="flex h-24 flex-col justify-around text-right">
-          <p>1 stop</p> <p>2h 45m in HNL</p>
-        </div>
-        <div className="flex h-24 flex-col justify-around text-right">
-          <p>Rs. 5894</p> <p>single trip</p>
-        </div>
-      </div>
-      <hr />
+      <FlightInfoCard />
+      <FlightInfoCard />
+      <FlightInfoCard />
     </section>
+  );
+}
+
+function FlightInfoCard() {
+  return (
+    <>
+      <div className="flex h-24 flex-row items-center justify-between">
+        <div className="">
+          <img src="/images/flightLogo.png" alt="" />
+        </div>
+        <div className="flex h-24 flex-col justify-around text-left">
+          <p>16h 23M</p> <p>Hawaiian Airlines</p>
+        </div>
+        <div className="flex h-24 flex-col justify-around text-left">
+          <p>7:00AM - 4:15PM</p> <p>Value</p>
+        </div>
+        <div className="flex h-24 flex-col justify-around text-right">
+          <p>1 stop</p> <p>2h 45m in HNL</p>
+        </div>
+        <div className="flex h-24 flex-col justify-around text-right">
+          <p>Rs. 5894</p> <p>single trip</p>
+        </div>
+      </div>
+      <hr />
+    </>
   );
 }
 
