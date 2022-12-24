85deac7b40090766525494fd2d94d342f14ef7df
Max Programming <ui.sabuwala@gmail.com>
2022-12-24 19:56:30 +0530

    Fix putting data in flights page bug

--- src/components/FlightsPage/FlightInfo.tsx
+++ src/components/FlightsPage/FlightInfo.tsx
@@ -12,31 +12,38 @@ function FlightInfoCard({ flights }: { flights: Flight[] }) {
   console.log(flights);
   return (
     <>
-      {flights.map((flight) => (
-        <div
-          key={flight.id}
-          className="flex flex-row items-center justify-between py-4"
-        >
-          <div className="">
-            <img src="/images/flightLogo.png" alt="" />
-          </div>
-          <div className="flex flex-col justify-around gap-4 text-left">
-            <p>{flight.itineraries[0]?.duration}</p> <p>Hawaiian Airlines</p>
-          </div>
-          <div className="flex flex-col justify-around gap-4 text-left">
-            <p>7:00AM - 4:15PM</p> <p>Value</p>
-          </div>
-          {flight.itineraries[0] && (
-            <div className="flex flex-col justify-around gap-4 text-right">
-              <p>{flight.itineraries[0]?.segments.length - 1} stop</p>{" "}
-              <p>2h 45m in HNL</p>
+      {flights.length !== 0 ? (
+        flights.map((flight) => (
+          <div
+            key={flight.id}
+            className="flex flex-row items-center justify-between py-4"
+          >
+            <div className="">
+              <img src="/images/flightLogo.png" alt="" />
             </div>
-          )}
-          <div className="flex flex-col justify-around gap-4 text-right">
-            <p>${flight.travelerPricings[0]?.price.total}</p> <p>single trip</p>
+            <div className="flex flex-col justify-around gap-4 text-left">
+              <p>{flight.itineraries[0]?.duration}</p> <p>Hawaiian Airlines</p>
+            </div>
+            <div className="flex flex-col justify-around gap-4 text-left">
+              <p>7:00AM - 4:15PM</p> <p>Value</p>
+            </div>
+            {flight.itineraries[0] && (
+              <div className="flex flex-col justify-around gap-4 text-right">
+                <p>{flight.itineraries[0]?.segments.length - 1} stop</p>{" "}
+                <p>2h 45m in HNL</p>
+              </div>
+            )}
+            <div className="flex flex-col justify-around gap-4 text-right">
+              <p>${flight.travelerPricings[0]?.price.total}</p>{" "}
+              <p>single trip</p>
+            </div>
           </div>
+        ))
+      ) : (
+        <div>
+          <p>No Flights Found</p>
         </div>
-      ))}
+      )}
       <hr />
     </>
   );
--- src/pages/flights.tsx
+++ src/pages/flights.tsx
@@ -21,6 +21,7 @@ export default function FlightsPage() {
   useEffect(() => {
     const lcData = localStorage.getItem("flightData");
     if (lcData) setFlightData(JSON.parse(lcData));
+    else setFlightData(null);
   }, []);
 
   console.log({ data });
@@ -29,7 +30,6 @@ export default function FlightsPage() {
       <Navbar />
       <section className="mb-20 flex flex-row items-center justify-evenly sm:w-screen">
         <div className="">
-
           <div className="my-4 flex justify-end sm:justify-center">
             <Link href="/passengerInfo">
               <button className="rounded-md bg-[#007CFF] p-4 text-white md:mr-4">
@@ -38,7 +38,7 @@ export default function FlightsPage() {
             </Link>
           </div>
 
-          <div className="flex justify-start sm:justify-center flex-row gap-2 lg:ml-4">
+          <div className="flex flex-row justify-start gap-2 lg:ml-4 sm:justify-center">
             <select name="" id="" className="rounded-lg border-2 p-2">
               <option value="Min Price" selected disabled>
                 Min Price
@@ -52,16 +52,20 @@ export default function FlightsPage() {
           </div>
 
           <div className="pb-12 pt-4">
-            {flightData && (
-              <FlightSearchInput
-                flightData={flightData}
-                setFlightData={setFlightData}
-              />
-            )}
+            <FlightSearchInput
+              flightData={flightData}
+              setFlightData={setFlightData}
+            />
           </div>
           <div className="flex flex-row items-center gap-6 sm:mt-10">
             <div className="border-gray flex overflow-y-scroll rounded-lg border-2 border-solid p-4">
-              {isLoading ? <div>Loading</div> : data ? <FlightInfo flights={data.flights!}/> : <div>No Flights right now</div>}
+              {isLoading ? (
+                <div>Loading</div>
+              ) : data ? (
+                <FlightInfo flights={data.flights!} />
+              ) : (
+                <div>No Flights right now</div>
+              )}
             </div>
             <div className="sm:hidden">
               <FlightsCart />
